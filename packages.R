7e11c135fc3aee9c17e150f2b1da41a71ab4993c
Izzy <isaiahkatz2020@u.northwestern.edu>
2025-07-02 16:19:28 +0200

    initial commit

---
+++ packages.R
@@ -0,0 +1,35 @@
+packages <- c(
+  "tidyverse", 
+  "dplyr",
+  "plotly", 
+  "reshape2",
+  "ggplot2",
+  "Matrix",
+  "vars",
+  "urca",
+  "zoo", 
+  "MASS", 
+  "splines",
+  "tsDyn",
+  "parallel",
+  "purrr",
+  "twosamples",
+  "CovTools", 
+  "Hotelling", 
+  "stats", 
+  "lubridate", 
+  "tseries", 
+  "YieldCurve", 
+  "dgof"
+)
+
+install_and_load <- function(pkg) {
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg)
+  }
+  suppressPackageStartupMessages(
+    library(pkg, character.only = TRUE)
+  )
+}
+
+invisible(lapply(packages, install_and_load))
