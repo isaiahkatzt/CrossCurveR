1d50cdf9dcb9e6ba16a7cd1c35707aa34fa8f959
Izzy <isaiahkatz2020@u.northwestern.edu>
2025-11-18 22:14:44 -0800

    single-day missing yield reconstruction

--- data_formatting/dr_formatting.R
+++ data_formatting/dr_formatting.R
@@ -49,10 +49,8 @@ dr_normalize_timestamp <- function(ts, tz = "UTC") {
   return(time) 
 }
 
-
 dr_yield_format <- function(data = "", mats = c(), start_date = "", end_date = "") {
-  ## reformat yields 
-  
+  ## format yields for later use 
   time <- .validate_input_data(data = data, mats = mats)
   mat_names <- sapply(mats, numeric_to_matname)
   data[, 1] <- time
@@ -60,7 +58,60 @@ dr_yield_format <- function(data = "", mats = c(), start_date = "", end_date = "
   colnames(data) <- c("time", mat_names)
 
   data <- data %>%
-    dplyr::filter(time >= as.Date(start_date) & time <= as.Date(end_date))
+    dplyr::filter(time >= as.Date(start_date) & time <= as.Date(end_date)) %>% 
+    mutate(time = strtrim(time, 10)) %>% 
+    mutate(across(everything(),
+                  ~ ifelse(is.nan(.), NA, .)))
   return(data)
 }
+
+#############################################
+#      Single-Day Missing Interpolation     #
+#############################################
+
+dr_daily <- function(yield_data, reference=NULL, tkey="time", threshold = 2){
+  
+  if (!is.null(reference)) {
+    ref_match <- match(reference, as.Date(yield_data[[tkey]]))
+    augmented <- matrix(NA, nrow = length(reference), ncol = ncol(yield_data))
+    augmented[!is.na(ref_match), ] <- as.matrix(yield_data[ref_match[!is.na(ref_match)], ])
+    augmented <- data.frame(augmented)
+    colnames(augmented) <- colnames(yield_data)
+    augmented[[tkey]] <- reference
+  } else {
+    augmented <- yield_data
+  }
+  
+  num_cols <- setdiff(colnames(augmented), tkey)
+  augmented[num_cols] <- lapply(augmented[num_cols], as.numeric)
+  
+  yield_matrix <- as.matrix(augmented[num_cols])
+  nr <- nrow(yield_matrix)
+  nc <- ncol(yield_matrix)
+  
+  sparse_yields <- rowSums(!is.na(yield_matrix)) < (threshold + 1)
+  preceding  <- rbind(matrix(NA, 1, nc), yield_matrix[-nr, ])
+  succeeding <- rbind(yield_matrix[-1, ], matrix(NA, 1, nc))
+  
+  int_mask <- sparse_yields & is.na(yield_matrix) & 
+    !is.na(preceding) & !is.na(succeeding)
+  
+  for (col_id in seq_len(nc)) {
+    row_mask <- int_mask[, col_id]
+    if (any(row_mask)) {
+      yprev <- preceding[row_mask,  col_id]
+      ynext <- succeeding[row_mask, col_id]
+      yield_matrix[row_mask, col_id] <- (yprev + ynext) / 2
+    }
+  }
+  
+  augmented[num_cols] <- yield_matrix
+  
+  return(augmented)
+}
+
+#############################################
+#           Spline-Based Bootstrap          #
+#############################################
+
 
