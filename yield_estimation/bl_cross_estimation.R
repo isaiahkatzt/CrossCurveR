2459cb1e7c9e5f4479f6254319cf3e026f674a79
Izzy <isaiahkatz2020@u.northwestern.edu>
2026-02-12 17:47:57 -0800

    mc fit exo

--- yield_estimation/bl_cross_estimation.R
+++ yield_estimation/bl_cross_estimation.R
@@ -57,7 +57,7 @@ mc_fit_end <- function(yields, lambdas, cutoffs, reference,
   P <- bln_build_P(curves, mats)  
   pY <- PY_full(Y[['tY']], P)
   
-  ## grid search lambda construction (to do: KF estimation) 
+  ## grid search lambda construction 
   curve_lambdas <- vapply(yields, function(x) { 
     lambda_grid_search(lambdas, x, mats)
   }, numeric(1) 
@@ -120,6 +120,84 @@ mc_fit_end <- function(yields, lambdas, cutoffs, reference,
     curve=ystar_curve, 
     sigma_JT=cc_Sigma,
     curveECM=cc_ci_features[['cc_ecm']], 
+    BS0=cc_BS0_feature, 
+    Xt=Xt))  
+}
+
+mc_fit_exo <- function(yields, lambdas, cutoffs, reference, Xt, X_normalize=TRUE,
+                       CR_algo=zero_mean_covreg, CR_init="adaptive", Xt_smooth=FALSE, smoother=c("ns", "bs"), knot_count=5,
+                       CR_maxiter=1000, CR_tol=1e-8, CR_Binit=NULL, CR_S0init=NULL, CR_verb=FALSE, CR_term=TRUE, 
+                       curves, mats) {
+  ## full exogenous covariate fit 
+  
+  ## build maturity x tenor strings 
+  mat_str <- sapply(mats, numeric_to_matname)
+  mds <- as.vector(t(outer(curves, mat_str, paste, sep='.')))
+  
+  ## build vector yield embedding and permutation matrix  
+  Y <- vecY(yields, curves, mats)   
+  ytime <- Y[['time']]
+  
+  P <- bln_build_P(curves, mats)  
+  pY <- PY_full(Y[['tY']], P)
+  
+  ## grid search lambda construction 
+  curve_lambdas <- vapply(yields, function(x) { 
+    lambda_grid_search(lambdas, x, mats)
+  }, numeric(1) 
+  )
+  
+  ## NS baseline estimation (no cross-curve) 
+  bl_yields <- transpose(setNames(lapply(seq_along(yields), function(x) {
+    bl_single_curve(curve_lambdas[x], yields[[x]], cutoffs, mats)
+  }
+  ), curves)) 
+  
+  ## residuals by tenor; within-curve full covariance  
+  phi_hat <- bln_phi_hat(bl_yields$phi, mds) 
+  W <- blcc_build_Wj(bl_yields$nsfit, mats) 
+  H <- bdiag(bl_yields$H) 
+  
+  if (Xt_smooth){
+    kps <- fe_kp_quantile(time=ytime, knot_count=knot_count)
+    Xt <- apply(Xt, 2, fe_xt_spline, time=ytime, spline_fit=smoother, knot_points=kps)
+  }
+  
+  ## covariance regression and sigma estimation 
+  cc_BS0_feature <- blcc_fe(Xt=Xt, Wt=W, mats=mats, covreg=CR_algo, init=CR_init, 
+                            max_iter=CR_maxiter, tol=CR_tol, S0=CR_S0init, B=CR_Binit, verb=CR_verb, term=CR_term) 
+  
+  cc_Sigma <- tSigma_optim(BS0=cc_BS0_feature, X=Xt)
+  
+  full_sigma_t <- sqrt_inv_build(cc_Sigma$tSigma)
+  sqrt_sigma_t <- full_sigma_t$sqrt
+  sqrt_inv_sigma_t <- full_sigma_t$inverse
+  
+  ## breve components 
+  yb <- bln_YB(pY[['py']], sqrt_inv_sigma_t) 
+  phib <- bln_phi_BT(phi_hat, P, sqrt_inv_sigma_t)
+  hb <- lapply(seq_along(sqrt_inv_sigma_t), function(dt) {
+    bln_Hb(H, P, sqrt_inv_sigma_t[[dt]])
+  })
+  
+  ## check components 
+  phic <- bln_phi_CT(phib, P) 
+  yc <- PY_full(yb, t(P))
+  hc <- lapply(hb, function(hb_dt) t(P) %*% hb_dt %*% P)
+  
+  ## curve cuts  
+  ycut <- cf_curve_cut(yc[['py']], curves, mats)
+  phicut <- cf_phi_cut(phic, curves, mats)
+  
+  ## normalized 
+  normalized <- bln_normalized_fit(ycut, phicut, curves, mats)
+  ystar_tenor <- blce_rescale_yield(normalized[['n_yield']], sqrt_sigma_t, P, "tenor", time=ytime, curves=curves, mats=mats)
+  ystar_curve <- blce_rescale_yield(normalized$n_yield, sqrt_sigma_t, P, "curve", time=ytime, curves=curves, mats=mats)
+  
+  return(list(
+    tenor=ystar_tenor, 
+    curve=ystar_curve, 
+    sigma_JT=cc_Sigma,
     BS0=cc_BS0_feature, 
     Xt=Xt))  
 }
